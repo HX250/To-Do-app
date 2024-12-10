71c913b2d83dc1b50dd41e38101808a5bb8bd008
Rastislav Pacut <rastislavpacut250@gmail.com>
2024-12-10 13:42:30 +0100

    updated img

--- src/app/features/todo/models/task.model.ts
+++ src/app/features/todo/models/task.model.ts
@@ -5,3 +5,21 @@ export interface task {
   state: boolean;
   date: string;
 }
+export class Task {
+  static clone(task: Task): Task {
+    return new Task(
+      task.id,
+      task.title,
+      task.description,
+      task.state,
+      task.date,
+    );
+  }
+  constructor(
+    public id: number = 1,
+    public title: string = '',
+    public description: string = '',
+    public state: boolean = false,
+    public date: string = '',
+  ) {}
+}
